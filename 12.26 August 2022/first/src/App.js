e865ee626c79ce56db46c016150a3534907bd74a
arpittyagi0748 <arpit3698.be20@chitkara.edu.in>
2022-09-06 09:41:36 +0530

    26 August 2022

---
+++ 12.26 August 2022/first/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header1 from './components/Header1';
+
+
+function App() {
+  return (
+  <div className='container'>
+  <Header1 title = 'gfdyta'/>
+  </div>
+  );
+ }
+
+export default App;
---
+++ 12.26 August 2022/first/src/components/Header1.js
@@ -0,0 +1,10 @@
+
+// import './Header.css';
+const Header1 = (props) =>{
+    return (
+        <header>
+        <h1>Hello, {props.title}</h1>
+        </header>
+        );
+}
+export default Header1;
